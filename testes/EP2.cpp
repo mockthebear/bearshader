81423ca88da2f6cea45a60e640a8bb0ec31a6ca5
Matheus Braga <matheus.mtb7@gmail.com>
2014-11-03 17:50:12 -0200

    a
    a

--- testes/EP2.cpp
+++ testes/EP2.cpp
@@ -26,51 +26,32 @@ typedef struct myParameter_{
 } myParameter;
 
 myParameter *params;
+pthread_barrier_t our_barrier;
 
+pthread_mutex_t lock;
 void *genPI(void *param)
 {
-       std::uniform_int_distribution<int> raar(0, 10000000);
-       std::mt19937 RRENG;
-       auto dice_roller = std::bind(raar, RRENG);
+       //pthread_barrier_wait(&our_barrier);
 
        myParameter *P = (myParameter*)param;
+       pthread_mutex_lock(&lock);
+       std::cout << "Hi tred" << P->mythread << "\n";
+       pthread_mutex_unlock(&lock);
 
-       P->inside = 0;
-       int test = 0;
-       #pragma omp parallel for num_threads(ompthreads) reduction(+ : test)
-        for (int i=0;i<P->toss;i++){
-            float XX = dice_roller()/10000000.0;
-            float YY = dice_roller()/10000000.0;
-            if (XX*XX + YY*YY <= 1){
-               test++;
-            }
-        }
-        P->inside = test;
 
        pthread_exit(NULL);
 }
 
 int main (int argc, char *argv[]) {
-    if (argc <= 1){
-        return 0;
-	}
-	for (int i=0;i<argc;i++){
-        if (argv[i][1] == 't'){
-            sscanf(argv[i],"-t=%d",&threads);
-        }
-        if (argv[i][1] == 'p'){
-            sscanf(argv[i],"-p=%d",&ompthreads);
-        }
-        if (argv[i][1] == 's'){
-            sscanf(argv[i],"-s=%d",&seed);
-        }
-        if (argv[i][1] == 'c'){
-            sscanf(argv[i],"-c=%ld",&tosses);
-        }
-	}
-
+    pthread_barrier_init(&our_barrier,NULL,4);
+   if (pthread_mutex_init(&lock, NULL) != 0)
+    {
+        printf("\n mutex init failed\n");
+        return 1;
+    }
 
 	gettimeofday(&start, NULL);
+	threads = 4;
 	threads_ = new  pthread_t[threads];
 	params = new myParameter[threads];
 	float *ret =new float[threads];
@@ -79,8 +60,6 @@ int main (int argc, char *argv[]) {
 	}
     for (int i = 0; i < threads; i++)
     {
-        params[i].seed = seed + i;
-        params[i].toss = tosses/(float)threads;
         params[i].mythread = i;
         pthread_create(&threads_[i], NULL, genPI, (void *) &params[i]);
     }
@@ -88,25 +67,9 @@ int main (int argc, char *argv[]) {
     {
         pthread_join(threads_[i], NULL);
     }
-    int N = 0;
-
-    for (int i = 0; i < threads; i++)
-    {
-        N += params[i].inside;
-    }
-    gettimeofday(&end, NULL);
 
 
 
-
-    std::fstream file("static.txt", std::ios::out | std::ios::app);
-    //
-    double dur = (end.tv_sec - start.tv_sec) + ( (end.tv_usec - start.tv_usec )/100000000.0);
-
-    std::cout << "Value of pi is around: " << ((double)N/(float)tosses)*4.0 << "\nFinished in " << dur <<"\n";
-    file << "[" << seed << "," << tosses << "," << threads << ","  << ((double)N/(float)tosses)*4.0 << "=" << (double)dur << "]" << std::endl;
-    //
-    file.close();
 
     return 1;
 }
